7f4c442256d456cc8ea4a7f17a66bbee41218391
iamfurkan19 <furkan199919@gmail.com>
2026-07-27 19:32:08 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const CACHE_NAME = "bodysync-v0.3";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json",
+    "./assets/icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.addAll(APP_FILES))
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return Promise.all(
+                    cacheNames
+                        .filter(
+                            (cacheName) =>
+                                cacheName !== CACHE_NAME
+                        )
+                        .map(
+                            (cacheName) =>
+                                caches.delete(cacheName)
+                        )
+                );
+            })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                const responseCopy = response.clone();
+
+                caches
+                    .open(CACHE_NAME)
+                    .then((cache) => {
+                        cache.put(
+                            event.request,
+                            responseCopy
+                        );
+                    });
+
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
