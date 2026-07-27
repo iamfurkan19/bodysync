1b291ca0663836ad36d9aee5ab5535e930ee8613
iamfurkan19 <furkan199919@gmail.com>
2026-07-27 19:24:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("BodySync gestartet");
+const dailyGoals = {
+    calories: 2000,
+    protein: 160
+};
+
+const dailyValues = {
+    calories: 0,
+    protein: 0
+};
+
+function updateDashboard() {
+    const caloriesPercent = Math.min(
+        (dailyValues.calories / dailyGoals.calories) * 100,
+        100
+    );
+
+    const proteinPercent = Math.min(
+        (dailyValues.protein / dailyGoals.protein) * 100,
+        100
+    );
+
+    document.getElementById("caloriesValue").textContent =
+        `${dailyValues.calories} / ${dailyGoals.calories}`;
+
+    document.getElementById("proteinValue").textContent =
+        `${dailyValues.protein} / ${dailyGoals.protein}`;
+
+    document.getElementById("caloriesProgress").style.width =
+        `${caloriesPercent}%`;
+
+    document.getElementById("proteinProgress").style.width =
+        `${proteinPercent}%`;
+}
+
+updateDashboard();
